5641fe0e1dce5279a16f6858e38c7f7c72b60e12
Chelsea Yang <chelsea.yang8@gmail.com>
2024-04-16 12:35:27 -0600

    Add project files.

---
+++ GymManagement/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace GymManagement
+{
+	public partial class App : Application
+	{
+		public App()
+		{
+			InitializeComponent();
+
+			MainPage = new MainPage();
+		}
+	}
+}
---
+++ GymManagement/MainPage.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GymManagement
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+	}
+}
